606d45e68c91ae8c08b0393452dbf9ce900a2234
jdgonzalezd1 <60698448+jdgonzalezd1@users.noreply.github.com>
2024-09-19 20:17:56 -0500

    Card initial setup

---
+++ Assets/Scripts/Cards/Template/Card.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "card", menuName = "Cards/Empty Card")]
+public class Card : ScriptableObject
+{
+    public string cardName;
+
+    public string cardDescription;
+
+    public Sprite splashArt;
+
+    public int flowerCost;
+
+    public int seedCost;
+
+    public int waterCost;
+}
